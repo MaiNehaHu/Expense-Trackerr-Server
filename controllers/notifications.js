ed69d4b8186ee869c9e9b0cc2506cb68a03ca906
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-01-22 13:41:46 +0530

    notifications by month

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -46,4 +46,36 @@ const getTodayNotifications = async (req, res) => {
     }
 };
 
-module.exports = { getAllNotifications, getTodayNotifications }
+const getMonthNotifications = async (req, res) => {
+    const { id: userId } = req.params;
+
+    try {
+        // Get the start and end of the current month
+        const startOfMonth = new Date();
+        startOfMonth.setDate(1); // Set to the first day of the month
+        startOfMonth.setHours(0, 0, 0, 0); // Reset the time to the start of the day
+
+        const endOfMonth = new Date();
+        endOfMonth.setMonth(endOfMonth.getMonth() + 1); // Move to the next month
+        endOfMonth.setDate(0); // Set to the last day of the current month
+        endOfMonth.setHours(23, 59, 59, 999); // Set time to the end of the day
+
+        // Find the user and filter notifications by the current month
+        const user = await User.findOne({ userId }).populate({
+            path: 'notifications',
+            match: { createdAt: { $gte: startOfMonth, $lte: endOfMonth } },
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Return only the notifications for the current month
+        res.status(200).json(user.notifications);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Error Getting Notifications', error });
+    }
+};
+
+module.exports = { getAllNotifications, getTodayNotifications, getMonthNotifications }
--- routes/notifications.js
+++ routes/notifications.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllNotifications, getTodayNotifications } = require('../controllers/notifications');
+const { getAllNotifications, getTodayNotifications, getMonthNotifications } = require('../controllers/notifications');
 
 router.route('/:id').get(getAllNotifications)
 
 router.route('/today/:id').get(getTodayNotifications)
+
+router.route('/month/:id').get(getMonthNotifications)
 
 module.exports = router;
