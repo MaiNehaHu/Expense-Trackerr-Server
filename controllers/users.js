886f2944d2d8414d9365e611582d7ad5589a51f3
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-03-19 10:59:30 +0530

    delete user with details

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,12 @@
 const User = require("../model/user");
 const Category = require("../model/category");
 const People = require("../model/people");
+const Transaction = require("../model/transaction");
+const RecuringTransaction = require("../model/recuringTransaction");
+const Notification = require('../model/notification');
+const Budget = require('../model/budget');
+const Trash = require('../model/trash')
+const mongoose = require("mongoose");
 
 // Utility function to generate unique IDs
 function generateUniqueId() {
@@ -95,18 +101,46 @@ async function editUser(req, res) {
 }
 
 async function deleteUser(req, res) {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const { id } = req.params;
-    // add logic to delete user image
-    // logic to delete all images of transactions
+    const user = await User.findOne({ userId: id }).session(session);
 
-    const user = await User.findOneAndDelete({ userId: id });
     if (!user) {
+      await session.abortTransaction();
+      session.endSession();
       return res.status(404).json({ error: "User not found" });
     }
 
-    res.status(200).json({ message: "User deleted successfully" });
+    const transactionIds = user.transactions.map(t => t._id);
+    const recurringTransactionIds = user.recuringTransactions.map(rt => rt._id);
+    const categoryIds = user.categories.map(c => c._id);
+    const peopleIds = user.people.map(p => p._id);
+    const notificationIds = user.notifications.map(n => n._id);
+    const budgetIds = user.budgets.map(b => b._id);
+    const trashIds = user.trash.map(t => t._id);
+
+    // Delete all related documents
+    await Transaction.deleteMany({ _id: { $in: transactionIds } }).session(session);
+    await RecuringTransaction.deleteMany({ _id: { $in: recurringTransactionIds } }).session(session);
+    await Category.deleteMany({ _id: { $in: categoryIds } }).session(session);
+    await People.deleteMany({ _id: { $in: peopleIds } }).session(session);
+    await Notification.deleteMany({ _id: { $in: notificationIds } }).session(session);
+    await Budget.deleteMany({ _id: { $in: budgetIds } }).session(session);
+    await Trash.deleteMany({ _id: { $in: trashIds } }).session(session);
+
+    // Delete user
+    await User.findOneAndDelete({ userId: id }).session(session);
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(200).json({ message: "User and all related data deleted successfully" });
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
     console.error("Error deleting user:", error);
     res.status(500).json({ error: "Failed to delete user" });
   }
